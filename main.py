971afa68bffbbe461421800cf83b7e33ff31d5bd
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-03-16 15:23:00 +0000

    Add exception to catch invalid date input

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+import datetime
 
-year = input("Choose a year to travel back to:\n")
+get_date = True
+while get_date:
+    year = input("Choose a date to travel back to (YYYY-MM-DD):\n")
+
+    try:
+        InvDate = datetime.datetime.strptime(year, "%Y-%m-%d")
+        print("Date valid")
+        get_date = False
+    except ValueError:
+        print("Date invalid")
